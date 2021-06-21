bd808ff6db9bffe3fad145155b7451d59fc1d8d5
Md. Raisul Islam <52915635+Raisul191491@users.noreply.github.com>
2021-06-21 06:28:59 +0600

    First sit

---
+++ Disperse_Project2/app/src/main/java/com/example/disperse_project/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.disperse_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int Splash_Time = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent Home_page = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(Home_page);
+            }
+        },Splash_Time);
+    }
+}
