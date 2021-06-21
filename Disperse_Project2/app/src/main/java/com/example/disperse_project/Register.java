2a6fc93a684b81f795f3118f7bb3ee49d8a75718
Md. Raisul Islam <52915635+Raisul191491@users.noreply.github.com>
2021-06-22 00:08:00 +0600

    Creating Reagister page

---
+++ Disperse_Project2/app/src/main/java/com/example/disperse_project/Register.java
@@ -0,0 +1,15 @@
+package com.example.disperse_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        
+    }
+}
