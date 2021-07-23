4235fcc95ef8cd69cc11eb91f7ba2d9757818a18
Md. Raisul Islam <52915635+Raisul191491@users.noreply.github.com>
2021-07-23 06:35:26 +0600

    readjusting with profile page

---
+++ Disperse_Project2/app/src/main/java/com/example/disperse_project/profilepage.java
@@ -0,0 +1,14 @@
+package com.example.disperse_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class profilepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profilepage);
+    }
+}
