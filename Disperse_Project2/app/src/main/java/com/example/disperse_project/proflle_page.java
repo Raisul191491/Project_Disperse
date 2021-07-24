27c94b8b74a495c88ac7f7c7e1cf585b124895ad
Md. Raisul Islam <52915635+Raisul191491@users.noreply.github.com>
2021-07-24 23:28:00 +0600

    content acquire

--- Disperse_Project2/app/src/main/java/com/example/disperse_project/proflle_page.java
+++ Disperse_Project2/app/src/main/java/com/example/disperse_project/proflle_page.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class profilepage extends AppCompatActivity {
+public class proflle_page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profilepage);
+        setContentView(R.layout.activity_proflle_page);
     }
 }
